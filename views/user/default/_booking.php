23f33a4faac9fb17238fe7aeb3ae8ed6c110e7fb
Dasha190194 <dariyogienko@gmail.com>
2018-12-22 07:50:11 +0300

    add booking page

--- views/user/default/_booking.php
+++ views/user/default/_booking.php
@@ -1,17 +1,67 @@
-<div class="profile-container booking-container">
-<!--    <h2>Текущее бронирование</h2>-->
+<div class="profile-container booking-container hidden">
 
-
+    <h2>Текущее бронирование</h2>
+    <?php foreach ($orders as $order): ?>
+        <?php if($order->is_book): ?>
+            <div class="panel panel-default">
+                <div class="panel-title">
+                    <img class="card-img-top" src="/index.png" width="748px" height="340px">
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <span><?= $order->datetime_create ?></span>
+                        </div>
+                        <div class="col-md-6">
+                            <span class="pull-right"> <?= $order->price ?></span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-offset-6 col-md-6">
+                            <span class="pull-right">Карта VISA</span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="#" class="btn btn-default btn-block">Отменить бронирование</a>
+                        </div>
+                        <div class="col-md-6">
+                            <a href="#" class="btn btn-primary btn-block">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
+    <?php endforeach; ?>
 
     <h2>История аренды</h2>
         <?php foreach ($orders as $order): ?>
-            <div class="card">
-                <img class="card-img-top" src="/index.png" width="760px" height="340px">
-                <div class="card-body">
-                    <h5 class="card-title"> <?= $order->boat->name ?></h5>
-                    <p class="card-text"> <?= $order->boat->description ?></p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+            <?php if($order->is_paid): ?>
+                <div class="panel panel-default">
+                    <div class="panel-title">
+                        <img class="card-img-top" src="/index.png" width="748px" height="340px">
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <span><?= $order->datetime_create ?></span>
+                            </div>
+                            <div class="col-md-6">
+                                <span class="pull-right"> <?= $order->price ?></span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-offset-6 col-md-6">
+                                <span class="pull-right">Карта VISA</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <a href="#" class="btn btn-primary btn-block">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            <?php endif; ?>
         <?php endforeach; ?>
 </div>
