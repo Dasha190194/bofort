5e37903846702aeefeba81c4a83ba57bfc1c02b0
Dasha190194 <dariyogienko@gmail.com>
2019-02-11 06:04:11 +0300

    1) change image folder 2) debug off

--- config/params.php
+++ config/params.php
@@ -2,7 +2,7 @@
 
 return [
     'adminEmail' => 'dariyogienko@gmail.com',
-    'uploadsPath' => 'http://bofort.su/uploads',
+    'uploadsPath' => '/srv/bofort/uploads',
     'cloud_id' => 'pk_de50bf19d0acc03c97a4063abeb49',
     'cloud_private_key' => 'fbdaae91cc1cdf5152cb4fa55862df07',
 ];
