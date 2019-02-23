6957f5e72d051912c8c4ed546ce5b60dcabcdae4
Dasha190194 <dariyogienko@gmail.com>
2019-02-23 18:14:21 +0300

    1) bugfix

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -25,19 +25,19 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/jpeg', 'href' => Url::
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 <!--     Yandex.Metrika counter -->
-    <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-        ym(52490173, "init", {
-            id:52490173,
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-        });
-    </script>
+<!--    <script type="text/javascript" >-->
+<!--        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};-->
+<!--            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})-->
+<!--        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");-->
+<!---->
+<!--        ym(52490173, "init", {-->
+<!--            id:52490173,-->
+<!--            clickmap:true,-->
+<!--            trackLinks:true,-->
+<!--            accurateTrackBounce:true,-->
+<!--            webvisor:true-->
+<!--        });-->
+<!--    </script>-->
     <noscript><div><img src="https://mc.yandex.ru/watch/52490173" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!--     /Yandex.Metrika counter -->
 </head>
