13bb2c893f09e694495622eb2393f227d7b7752a
Dasha190194 <dariyogienko@gmail.com>
2019-03-24 15:55:18 +0300

    1) bugfix

--- views/layouts/mainHeader.php
+++ views/layouts/mainHeader.php
@@ -27,6 +27,9 @@
                     <?php  if (Yii::$app->user->isGuest): ?>
                         <a id="login" class="btn btn-default" onclick="return false;">Войти</a>
                     <?php else: ?>
+                        <?php if (Yii::$app->user->identity->isShipowner()): ?>
+                            <li><a class="btn btn-default" href="/admin">Админка</a></li>
+                        <?php endif; ?>
                         <a class="btn btn-default" href="/user/profile"><?= Yii::$app->user->identity->username?></a>
                     <?php endif; ?>
                 </li>
