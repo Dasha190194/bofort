b5ae4cc5054bb14dca95aa1c648d29081b261cbe
Dasha190194 <dariyogienko@gmail.com>
2019-03-24 17:42:51 +0300

    1) bugfix

--- views/user/default/_notifications.php
+++ views/user/default/_notifications.php
@@ -4,25 +4,6 @@
 
 <div class="profile-container notifications-container">
     <?php if (count($notifications) > 0): ?>
-        <h2>Уведомления - <?= count($notifications)?></h2> <span id="clear-notifications">Очистить всё</span>
-
-        <?php foreach ($notifications as $notification): ?>
-            <div class="row">
-                <div data-id="<?= $notification->id ?>" class="notification-panel panel panel-default <?php if ($notification->is_open == 0) echo 'noOpen';?>">
-                    <div class="panel-body">
-                        <?= $notification->text ?>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    <?php else: ?>
-        <strong>У вас нет новых уведомлений</strong>
-    <?php endif; ?>
-</div>
-
-
-<div class="profile-container notifications-container">
-    <?php if (count($notifications) > 0): ?>
         <div class="row">
             <div class="col-md-6"><h3>Уведомления &bull;  <?= count($notifications)?></h3></div>
             <div class="col-md-6 text-right"><span id="clear-notifications" class="clear-all">Очистить всё</span></div>
