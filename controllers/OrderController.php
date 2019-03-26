ac34fceca0613149bf6ad54f36bb3a74ff189d01
Dasha190194 <dariyogienko@gmail.com>
2019-03-26 18:53:33 +0300

    1) bugfix

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -381,6 +381,7 @@ class OrderController extends Controller
                     else $price += $tariff->four_hours_weekday;
                 }
                 elseif (in_array($datetimes[0]->format('D'), ['Sat', 'Sun'])) $price = $tariff->holiday;
+                elseif (in_array($datetimes[0]->format('d-m'), ['01-05', '02-05', '03-05', '09-05', '10-05', '12-06'])) $price += $tariff->holiday;
                 else $price = $tariff->weekday;
                 $price = $price*count($datetimes);
             }
