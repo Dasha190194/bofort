bffa690cfa810cc3b3764e8d880c0d1ed77cfcba
Dasha190194 <dariyogienko@gmail.com>
2019-04-08 19:21:35 +0300

    fix

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -69,6 +69,35 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/jpeg', 'href' => Url::
     </div>
 </div>
 
+
+<!--     Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym("<?= Yii::$app->params['yandex_metrika'] ?>", "init", {
+        id:<?= Yii::$app->params['yandex_metrika'] ?>,
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/<?= Yii::$app->params['yandex_metrika'] ?>" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!--     /Yandex.Metrika counter -->
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?= Yii::$app->params['google_analytics']?>"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', "<?= Yii::$app->params['google_analytics']?>");
+</script>
+
+
 <?php $this->endBody() ?>
 </body>
 </html>
