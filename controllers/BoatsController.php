73c81ae7da5651afe671f5261aba9577bebb4746
dogienko <d.ogienko@ticketland.ru>
2019-04-10 11:19:51 +0300

    1) bugfix

--- controllers/BoatsController.php
+++ controllers/BoatsController.php
@@ -63,10 +63,7 @@ class BoatsController extends Controller
                 if (Yii::$app->user->identity->isShipowner()) {
                     $boats = BoatsModel::find()->where(['user_id' => $shipowner])->all();
                 }
-            } elseif (!Yii::$app->user->isGuest and Yii::$app->user->identity->isAdmin()) {
-                $boats = BoatsModel::find()->all();
-            }
-            else $boats = BoatsModel::find()->where(['!=', 'category_id', 2])->all();
+            } else $boats = BoatsModel::find()->where(['!=', 'category_id', 2])->all();
         }
 
         return $this->render('index', compact('boats'));
